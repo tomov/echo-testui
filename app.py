89703b2708d63f2dcffd12cd31bfc0371633689c
Momchil Tomov <tomov90@gmail.com>
2013-03-18 22:54:50 -0400

    LOTS OF SHITS

--- app.py
+++ app.py
@@ -41,5 +41,5 @@ def page_not_found(e):
 #----------------------------------------
 
 if __name__ == "__main__":
-	port = int(os.environ.get("PORT", 5000))
+	port = int(os.environ.get("PORT", 5555))
 	app.run(host='0.0.0.0', port=port)
